a8ff690b5861c3b1234f64a927c318200c02b8d8
Luis Bonilla <boni800675@siigo.com>
2020-11-23 19:51:30 -0500

    implementacion de formularios

---
+++ src/Styled-Components/login.style.tsx
@@ -0,0 +1,10 @@
+import Styled from 'styled-components';
+
+/**
+ * Container login
+ */
+export const LoginContainer = Styled.div`
+    height: 100vh;
+`;
+
+
---
+++ src/components/Auth/Login/signin.component.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from 'react';
+import { Button, Form, Input } from 'antd';
+import { Store } from 'antd/lib/form/interface';
+import '../../../index.css';
+import { useHistory } from 'react-router-dom';
+/**
+ * Destructurin object
+ */
+const { Item } = Form;
+
+export const SignInComponent: FC = () => {
+
+  const history = useHistory();
+
+  //Hook
+  const [form] = Form.useForm();
+
+  const redirectRegister = () => history.push('/register');
+
+  const homereDirect = () => history.push('/');
+
+
+  const handleValues = async (values: Store) => {
+    console.log(values);
+    const response = await (await fetch('https://api-rest-unia-movie.herokuapp.com/Auth/SignIn', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ...values })
+    })).json()
+  }
+
+  return (
+    <div className="form-create">
+      <h1 className="title-form-create">Sign in</h1>
+      <Form onFinish={handleValues} form={form} layout='vertical'>
+        {/* Email */}
+        <Item label="Email" name="email" rules={[{ type: 'email', message: 'Format no valid' }, { required: true, message: 'Field requerid' }]}  >
+          <Input />
+        </Item>
+        {/* Password */}
+        <Item label="Password" name="password" rules={[{ required: true, message: 'Field requerid' }]}  >
+          <Input.Password />
+        </Item>
+        <Item>
+          <Button type="primary" htmlType="submit">
+            Continue
+          </Button>
+        </Item>
+      </Form>
+      <Button onClick={redirectRegister}>Register</Button>
+      <Button type='text' onClick={homereDirect}>Home</Button>
+    </div>
+  )
+}
